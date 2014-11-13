2cf619e5a16a51ec22916b8107f58b4d3318e7c4
ddfelts <dan.felts@gmail.com>
2014-11-13 13:49:25 -0600

    Updated COre again

--- hawkAPI/lib/core/hawkcore.py
+++ hawkAPI/lib/core/hawkcore.py
@@ -139,7 +139,7 @@ class hawkcore(object):
              ndata = ""
              try:
                 ndata = r.json()
-             except ConnectionError:
+             except requests.exceptions.ConnectionError:
                 self.doTest(api,data)
              if len(ndata) > 1:
                 if self.debugit == "True":
