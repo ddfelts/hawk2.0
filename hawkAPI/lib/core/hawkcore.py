6af57837001c335a74cf7a680e48e6d0bdc0cc18
ddfelts <dan.felts@gmail.com>
2014-11-13 18:44:15 -0600

    Updated Core

--- hawkAPI/lib/core/hawkcore.py
+++ hawkAPI/lib/core/hawkcore.py
@@ -137,7 +137,8 @@ class hawkcore(object):
                    return 0
                ndata = ""
                try:
-                   ndata = r.json().decode('utf-8')
+                   ndata = json.loads(r.text).decode('utf-8')
+                   #ndata = r.json().decode('utf-8')
                except requests.exceptions.RequestException,e:
                    print e
  
